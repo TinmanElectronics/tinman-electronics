d3ab6a0727c1f44193a15913a2abe18dc76cd974
Justin R. St-Amant <jstamant24@gmail.com>
2015-09-29 05:58:38 -0500

    Added the "Viewable With Any Browser" campaign

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,15 @@
     <div id="footer">
       <p>Website maintained by Justin R. St-Amant (jstamant at tinmanelectronics dot com)</p>
-      <a href="http://validator.w3.org/check?uri=referer">
-        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
-      </a>
+      <p>
+        <a href="http://validator.w3.org/check?uri=referer">
+          <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" width="88" height="31" />
+        </a>
+      </p>
+      <p>
+        <a href="http://www.anybrowser.org/campaign/">
+          <img src="/w3c-ab.png" alt="Viewable With Any Browser" width="88" height="31" />
+        </a>
+      </p>
     </div><!-- #footer -->
   </body>
 </html>
