2d8ad6ecc8efa393f9fd0ae92e140aade94c0a44
Justin R. St-Amant <jstamant24@gmail.com>
2015-10-01 17:13:41 -0500

    Halted master developement to test out Pico CMS

---
+++ 404.php
@@ -0,0 +1,4 @@
+<h1>404</h1>
+<p>
+  The page you have requested is not available.
+</p>
--- content/videos/index.php
+++ content/videos/index.php
@@ -1,7 +1,24 @@
-<h1>Videos</h1>
-
+<div>
+  <a href="https://www.youtube.com/channel/UCPK-tIF2tVaYF65psGn8Dlg">
+    <h1>Tinman Electronics Youtube Channel</h1>
+  </a>
+</div>
+<p>
+  Summary
+</p>
+<div class="video-showcase">
+  <h2>Tinman Electronics 7 - How to interface a quadrature output rotary encoder</h2>
+</div>
+<div class="video-showcase">
+  <h2>Tinman Electronics 15 - Modyfing the Fender Hot Rod Deluxe III</h2>
+</div>
+<div class="video-showcase">
+  <h2>Tinman Electronics 3 - First time lead-free solder user</h2>
+</div>
 <?php
 
+
+/*
 $video_contents = scandir($content_base . $path, SCANDIR_SORT_DESCENDING);
 
 //Loop through each video's directory
@@ -10,10 +27,16 @@ foreach ($video_contents as $video) {
     echo "<div class=\"video-post\">".PHP_EOL;
     //Print a summary and thumbnail for each video
     $parser = simplexml_load_file($path_parts['dirname'] . $path . "/".$video . "/index.xml");
-    echo "  <h2>" . $parser->title . "</h2>".PHP_EOL;
-    echo "  <img src=\"/content"."$path/$video/thumbnail.jpg\" alt=\"video thumbnail\" />".PHP_EOL;
-    echo "</div><!-- #$video -->".PHP_EOL;
+    echo "  <div class=\"thumbnail\">".PHP_EOL;
+    echo "    <img src=\"/content"."$path/$video/thumbnail.jpg\" alt=\"video thumbnail\" />".PHP_EOL;
+    echo "  </div><!-- .thumbnail -->".PHP_EOL;
+    echo "  <div class=\"summary\">".PHP_EOL;
+    echo "    <h2>" . $parser->title . "</h2>".PHP_EOL;
+    echo "    <p>" . "Some summary text retrieved by the XML parser." . "</p>".PHP_EOL;
+    echo "  </div><!-- .summary -->".PHP_EOL;
+    echo "</div><!-- .video-post -->".PHP_EOL;
   }
 }
+*/
 
 ?>
