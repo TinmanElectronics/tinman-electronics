d2048917aac5aed76f26de8d05f49327e92acd60
Justin R. St-Amant <jstamant24@gmail.com>
2015-10-01 17:14:07 -0500

    Halted master developement to test out Pico CMS

--- templates/nav.php
+++ templates/nav.php
@@ -1,9 +1,11 @@
 <div id="nav">
   <ul>
     <li><a href="/">Home</a></li>
+    <li><a href="/blog">Blog???</a></li>
+    <li><a href="/articles">Articles</a></li>
     <li><a href="/projects">Projects</a></li>
-    <li><a href="/about">About</a></li>
     <li><a href="/videos">Videos</a></li>
+    <li><a href="/about">About</a></li>
     <li><a href="/contact">Contact</a></li>
   </ul>
 </div><!-- #nav -->
