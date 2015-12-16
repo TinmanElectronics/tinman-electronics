878a4ada3fb8a549331aed44750477944b5dad5b
Justin R. St-Amant <jstamant24@gmail.com>
2015-12-16 15:42:12 -0600

    Modified my permissions-setting script.

--- reset-permissions.sh
+++ reset-permissions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Give all directories 775 permissions
-chmod 775 $(find /srv/http -type d)
+chmod 775 $(find /srv/http/te -type d)
 # Give all files 664 permissions
-chmod 664 $(find /srv/http -type f)
+chmod 664 $(find /srv/http/te -type f)
 
 # Change ownership of all files and directories for web developement
-chown root:srv $(find /srv/http)
+chown root:srv $(find /srv/http/te)
 
 # Reset executable permissions of this script
-chmod 775 /srv/http/reset-permissions.sh
+chmod 775 /srv/http/te/reset-permissions.sh
